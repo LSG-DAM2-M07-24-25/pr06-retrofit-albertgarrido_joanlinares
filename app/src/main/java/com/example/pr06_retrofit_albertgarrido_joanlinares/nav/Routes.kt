69cfdb248882561a6b16a6dfaf247db1cb5912d5
JoanLinares <joan.linares@gracia.lasalle.cat>
2025-01-31 17:28:53 +0100

    feat:  added routes

--- app/src/main/java/com/example/pr06_retrofit_albertgarrido_joanlinares/nav/Routes.kt
+++ app/src/main/java/com/example/pr06_retrofit_albertgarrido_joanlinares/nav/Routes.kt
@@ -1,4 +1,9 @@
 package com.example.pr06_retrofit_albertgarrido_joanlinares.nav
 
-class Routes {
+sealed class Routes(val route: String) {
+    object Screen1 : Routes("screen1")
+
+    object Screen2 : Routes("screen2") {
+    }
+
 }
