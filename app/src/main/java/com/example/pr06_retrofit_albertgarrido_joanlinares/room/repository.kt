6bd1401f95e845575c26262b4eea54f95a9a68eb
JoanLinares <joan.linares@gracia.lasalle.cat>
2025-03-03 19:32:03 +0100

    fix: repository fixed

--- app/src/main/java/com/example/pr06_retrofit_albertgarrido_joanlinares/room/repository.kt
+++ app/src/main/java/com/example/pr06_retrofit_albertgarrido_joanlinares/room/repository.kt
@@ -3,13 +3,19 @@ package com.example.pr06_retrofit_albertgarrido_joanlinares.room
 import com.example.pr06_retrofit_albertgarrido_joanlinares.model.Pokemon
 
 class Repository {
-    val daoInterface = PokemonApplication.database.pokemonDao()
+    private val daoInterface = PokemonApplication.database.pokemonDao()
 
-    suspend fun getCaptured(): MutableList<Pokemon> = daoInterface.getAddedToCart()
-    suspend fun findByName(pokemon: Pokemon) = daoInterface.findByName(pokemon.name).isNotEmpty()
-    suspend fun isCaptured(name: String): Boolean = daoInterface.isCaptured(name)
-    suspend fun capturePokemon(pokemon: Pokemon) = daoInterface.capturePokemon(pokemon)
-    suspend fun freePokemon(pokemon: Pokemon) = daoInterface.freePokemon(pokemon)
-    suspend fun updateCapturedStatus(name: String, isCaptured: Boolean) = daoInterface.updateCapturedStatus(name, isCaptured)
+    suspend fun getAddedToCart(): MutableList<Pokemon> = daoInterface.getAddedToCart()
 
+    suspend fun findByName(pokemon: Pokemon): Boolean =
+        daoInterface.findByName(pokemon.name).isNotEmpty()
+
+    suspend fun isAddedToCart(name: String): Boolean = daoInterface.isAddedToCart(name)
+
+    suspend fun addPokemonToCart(pokemon: Pokemon) = daoInterface.addPokemonToCart(pokemon)
+
+    suspend fun removePokemonFromCart(pokemon: Pokemon) = daoInterface.removePokemonFromCart(pokemon)
+
+    suspend fun updateAddedToCartStatus(name: String, isAdded: Boolean) =
+        daoInterface.updateAddedToCartStatus(name, isAdded)
 }
