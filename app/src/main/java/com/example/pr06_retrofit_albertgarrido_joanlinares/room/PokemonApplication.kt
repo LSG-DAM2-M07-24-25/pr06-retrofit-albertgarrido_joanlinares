227565dfdef377660e45c98e69505e989ca0d786
JoanLinares <joan.linares@gracia.lasalle.cat>
2025-03-08 11:48:58 +0100

    fix: delete unused imports

--- app/src/main/java/com/example/pr06_retrofit_albertgarrido_joanlinares/room/PokemonApplication.kt
+++ app/src/main/java/com/example/pr06_retrofit_albertgarrido_joanlinares/room/PokemonApplication.kt
@@ -1,18 +1,17 @@
 package com.example.pr06_retrofit_albertgarrido_joanlinares.room
 
 import android.app.Application
-import androidx.room.Room
 import androidx.room.Room.databaseBuilder
 
 class PokemonApplication : Application() {
-    // Creem un atribut estàtic de la classe
+    // Atributo estático de la clase
     companion object {
         lateinit var database: PokemonDatabase
     }
 
     override fun onCreate() {
         super.onCreate()
-        // Aquí determinem el nom de la base de dades. En el nostre cas "PokemonDatabase"
+        // nombre base de datos
         database = databaseBuilder(this, PokemonDatabase::class.java,"PokemonDatabase").build()
     }
 }
